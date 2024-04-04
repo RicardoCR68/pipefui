a2a6e4d80bf1d91980efc1d29f450c0c930f8087
RicardoCR68 <ricardoc6r8@gmail.com>
2024-04-04 16:22:42 -0300

    answer specs in progress

--- spec/requests/answer_spec.rb
+++ spec/requests/answer_spec.rb
@@ -1,53 +1,73 @@
 require 'rails_helper'
 
-RSpec.describe "Answers", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/answer/index"
-      expect(response).to have_http_status(:success)
+RSpec.describe 'Answers', type: :request do
+  subject(:form) { create(:form) }
+
+  describe 'GET /index' do
+    before { get form_answers_path(form) }
+
+    it { expect(response).to be_successful }
+  end
+
+  describe 'GET /new' do
+    before { get new_form_answer_path(form) }
+
+    it { expect(response).to be_successful }
+  end
+
+  describe 'POST /create' do
+    context 'with valid params' do
+      let (:params) { { answer: attributes_for(:answer) } }
+
+      it { expect { post form_answers_path(form), params: }.to change { Answer.count }.by(1) }
+    end
+
+    context 'with invalid params' do
+      let (:params) { { answer: { email: nil } } }
+
+      it { expect { post form_answers_path(form), params: }.not_to change(Answer, :count) }
     end
   end
 
-  describe "GET /new" do
-    it "returns http success" do
-      get "/answer/new"
-      expect(response).to have_http_status(:success)
-    end
+  describe 'GET /show' do
+    let(:answer) { create(:answer, form:) }
+
+    before { get form_answer_path(form, answer) }
+
+    it { expect(response).to be_successful }
+    it { expect(response.body).to include(answer.email) }
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/answer/create"
-      expect(response).to have_http_status(:success)
-    end
+  describe 'GET /edit' do
+    let(:answer) { create(:answer, form:) }
+
+    before { get edit_form_answer_path(form, answer) }
+
+    it { expect(response).to be_successful }
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/answer/show"
-      expect(response).to have_http_status(:success)
-    end
+  describe 'PATCH /update' do
+    let(:params) { { answer: attributes_for(:answer) } }
+    let(:answer) { create(:answer, form:) }
+
+    before { expect { patch form_answer_path(form, answer), params: } }
+
+    it { expect(answer,reload.email).to eq(params[:answer][:email]) }
   end
 
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/answer/edit"
-      expect(response).to have_http_status(:success)
-    end
+  describe 'PUT /update' do
+    let(:params) { { answer: attributes_for(:answer) } }
+    let(:answer) { create(:answer, form:) }
+
+    before { expect { put form_answer_path(form, answer), params: } }
+
+    it { expect(answer,reload.email).to eq(params[:answer][:email]) }
   end
 
-  describe "GET /update" do
-    it "returns http success" do
-      get "/answer/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  describe 'DELETE /destroy' do
+    let!(:answer) { create(:answer, form:) }
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/answer/destroy"
-      expect(response).to have_http_status(:success)
-    end
+    it { expect{ delete form_answer_path(form, answer) }.to change { Answer.count }.by(-1) }
   end
 
 end
